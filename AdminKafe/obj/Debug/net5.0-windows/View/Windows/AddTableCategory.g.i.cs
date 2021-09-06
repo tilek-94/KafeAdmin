2bd49bc4e79364aac22cebe8730d36246ed23571
daniyarabdulhamidov@outlook.com <daniyarabdulhamidov@outlook.com>
2021-09-06 13:00:10 +0300

    fff

--- AdminKafe/obj/Debug/net5.0-windows/View/Windows/AddTableCategory.g.i.cs
+++ AdminKafe/obj/Debug/net5.0-windows/View/Windows/AddTableCategory.g.i.cs
@@ -67,13 +67,13 @@ namespace AdminKafe.View.Windows {
         /// InitializeComponent
         /// </summary>
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "5.0.9.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "6.0.0.0")]
         public void InitializeComponent() {
             if (_contentLoaded) {
                 return;
             }
             _contentLoaded = true;
-            System.Uri resourceLocater = new System.Uri("/AdminKafe;V1.0.0.0;component/view/windows/addtablecategory.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/AdminKafe;component/view/windows/addtablecategory.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\..\..\View\Windows\AddTableCategory.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
@@ -83,7 +83,7 @@ namespace AdminKafe.View.Windows {
         }
         
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "5.0.9.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "6.0.0.0")]
         [System.ComponentModel.EditorBrowsableAttribute(System.ComponentModel.EditorBrowsableState.Never)]
         [System.Diagnostics.CodeAnalysis.SuppressMessageAttribute("Microsoft.Design", "CA1033:InterfaceMethodsShouldBeCallableByChildTypes")]
         [System.Diagnostics.CodeAnalysis.SuppressMessageAttribute("Microsoft.Maintainability", "CA1502:AvoidExcessiveComplexity")]
--- AdminKafe/obj/Debug/net5.0-windows/View/Windows/MessageWindowOk.g.i.cs
+++ AdminKafe/obj/Debug/net5.0-windows/View/Windows/MessageWindowOk.g.i.cs
@@ -58,13 +58,13 @@ namespace AdminKafe.View.Windows {
         /// InitializeComponent
         /// </summary>
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "5.0.9.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "6.0.0.0")]
         public void InitializeComponent() {
             if (_contentLoaded) {
                 return;
             }
             _contentLoaded = true;
-            System.Uri resourceLocater = new System.Uri("/AdminKafe;V1.0.0.0;component/view/windows/messagewindowok.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/AdminKafe;component/view/windows/messagewindowok.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\..\..\View\Windows\MessageWindowOk.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
@@ -74,7 +74,7 @@ namespace AdminKafe.View.Windows {
         }
         
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "5.0.9.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "6.0.0.0")]
         [System.ComponentModel.EditorBrowsableAttribute(System.ComponentModel.EditorBrowsableState.Never)]
         [System.Diagnostics.CodeAnalysis.SuppressMessageAttribute("Microsoft.Design", "CA1033:InterfaceMethodsShouldBeCallableByChildTypes")]
         [System.Diagnostics.CodeAnalysis.SuppressMessageAttribute("Microsoft.Maintainability", "CA1502:AvoidExcessiveComplexity")]
